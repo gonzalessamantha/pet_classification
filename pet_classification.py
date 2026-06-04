733bc7d5a632dc0a0e4f15f5bbaa733f255c55d9
unknown <gonzalessamanthagrace07@gmail.com>
2026-06-04 18:00:05 +0800

    I've added the the program that adds object to the class

--- pet_classification.py
+++ pet_classification.py
@@ -22,3 +22,24 @@ class Pet:
     def get_age(self):
         return self.__age
 
+def main():
+    my_pet = Pet()
+
+    name = input("Enter pet name: ")
+    animal_type = input("Pet's animal type: ")
+    age_str = input("Pet's age in integer: ")
+    age = int(age_str) \
+        if age_str.isdigit() \
+        else 0
+
+    my_pet.set_name(name)
+    my_pet.set_animal_type(animal_type)
+    my_pet.set_age(age)
+
+    print("\n*** PET INFORMATION ***")
+    print(f"Name:        \033[1m\033[33m{my_pet.get_name()}\033[0m")
+    print(f"Animal Type: \033[1m\033[33m{my_pet.get_animal_type()}\033[0m")
+    print(f"Age:         \033[1m\033[33m{my_pet.get_age()}\033[0m")
+
+if __name__ == "__main__":
+    main()
